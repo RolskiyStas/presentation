55e28f9d72fc2a2cb1b1542213e5690bfe26dadb
RolskyiStas <stas.rolskiy@gmail.com>
2022-08-02 14:28:04 +0300

    delet _switch.js

--- js/main.js
+++ js/main.js
@@ -72,5 +72,29 @@ function asideSectionToggleBtn() {
 	for(let i = 0; i < totalSection; i++) {
 		allSection[i].classList.toggle('open')
 	}
+}
 
+/* toggle style switcher */
+const styleSwitcherToggle = document.querySelector('.switcher__toggler')
+styleSwitcherToggle.addEventListener('click', () => {
+	document.querySelector('.switcher').classList.toggle('open')
+})
+
+window.addEventListener('scroll', () => {
+	if (document.querySelector('.switcher').classList.contains('open')) {
+		document.querySelector('.switcher').classList.remove('open')
+	}
+})
+
+/* theme colors */
+
+const alternateStyle = document.querySelectorAll('.alternate-style')
+function setActiveStyle(color) {
+	alternateStyle.forEach((style) => {
+		if (color === style.getAttribute('title')) {
+			style.removeAttribute('disabled')
+		} else {
+			style.setAttribute('disabled','true')
+		}
+	})
 }
